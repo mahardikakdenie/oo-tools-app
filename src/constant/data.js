44c6eb2123e01bbe094095437838579250ca5a63
MahardiikaKD <dikamahar884@gmail.com>
2023-03-04 11:56:04 +0700

    init home

--- src/constant/data.js
+++ src/constant/data.js
@@ -152,44 +152,12 @@ export const message = [
 export const topMenu = [
   {
     isHeadr: true,
-    title: "DASHBOARD",
+    title: "menu",
   },
   {
     title: "Dashboard",
     icon: "heroicons-outline:home",
-    link: "/app/home",
-    child: [
-      {
-        childtitle: "Analytics Dashboard",
-        childlink: "home",
-        childicon: "heroicons:presentation-chart-line",
-      },
-      {
-        childtitle: "Ecommerce Dashboard",
-        childlink: "ecommerce",
-        childicon: "heroicons:shopping-cart",
-      },
-      {
-        childtitle: "Project  Dashboard",
-        childlink: "project",
-        childicon: "heroicons:briefcase",
-      },
-      {
-        childtitle: "CRM Dashboard",
-        childlink: "crm",
-        childicon: "ri:customer-service-2-fill",
-      },
-      {
-        childtitle: "Banking Dashboard",
-        childlink: "banking",
-        childicon: "heroicons:wrench-screwdriver",
-      },
-    ],
-  },
-  {
-    title: "App",
-    icon: "heroicons-outline:chip",
-    link: "/app/home",
+    link: "home",
     child: [
       {
         childtitle: "Calendar",
@@ -213,271 +181,337 @@ export const topMenu = [
       },
     ],
   },
-  {
-    title: "Pages",
-    icon: "heroicons-outline:view-boards",
-    link: "/app/home",
-    megamenu: [
-      {
-        megamenutitle: "Authentication",
-        megamenuicon: "heroicons-outline:user",
-        singleMegamenu: [
-          {
-            m_childtitle: "Signin One",
-            m_childlink: "/",
-          },
-          {
-            m_childtitle: "Signin Two",
-            m_childlink: "/login2",
-          },
-          {
-            m_childtitle: "Signin Three",
-            m_childlink: "/login3",
-          },
-          {
-            m_childtitle: "Signup One",
-            m_childlink: "/register",
-          },
-          {
-            m_childtitle: "Signup Two",
-            m_childlink: "/register/register2",
-          },
-          {
-            m_childtitle: "Signup Three",
-            m_childlink: "/register/register3",
-          },
-          {
-            m_childtitle: "Forget Password One",
-            m_childlink: "/forgot-password",
-          },
-          {
-            m_childtitle: "Forget Password Two",
-            m_childlink: "/forgot-password2",
-          },
-          {
-            m_childtitle: "Forget Password Three",
-            m_childlink: "/forgot-password3",
-          },
-          {
-            m_childtitle: "Lock Screen One",
-            m_childlink: "/lock-screen",
-          },
-          {
-            m_childtitle: "Lock Screen Two",
-            m_childlink: "/lock-screen2",
-          },
-          {
-            m_childtitle: "Lock Screen Three",
-            m_childlink: "/lock-screen3",
-          },
-        ],
-      },
-
-      {
-        megamenutitle: "Components",
-        megamenuicon: "heroicons-outline:user",
-        singleMegamenu: [
-          {
-            m_childtitle: "typography",
-            m_childlink: "typography",
-          },
-          {
-            m_childtitle: "colors",
-            m_childlink: "colors",
-          },
-          {
-            m_childtitle: "alert",
-            m_childlink: "alert",
-          },
-          {
-            m_childtitle: "button",
-            m_childlink: "button",
-          },
-          {
-            m_childtitle: "card",
-            m_childlink: "card",
-          },
-          {
-            m_childtitle: "carousel",
-            m_childlink: "carousel",
-          },
-          {
-            m_childtitle: "dropdown",
-            m_childlink: "dropdown",
-          },
-          {
-            m_childtitle: "image",
-            m_childlink: "image",
-          },
-          {
-            m_childtitle: "modal",
-            m_childlink: "modal",
-          },
-          {
-            m_childtitle: "Progress bar",
-            m_childlink: "progress-bar",
-          },
-          {
-            m_childtitle: "Placeholder",
-            m_childlink: "placeholder",
-          },
-
-          {
-            m_childtitle: "Tab & Accordion",
-            m_childlink: "tab-accordion",
-          },
-        ],
-      },
-      {
-        megamenutitle: "Forms",
-        megamenuicon: "heroicons-outline:user",
-        singleMegamenu: [
-          {
-            m_childtitle: "Input",
-            m_childlink: "input",
-          },
-          {
-            m_childtitle: "Input group",
-            m_childlink: "input-group",
-          },
-          {
-            m_childtitle: "Input layout",
-            m_childlink: "input-layout",
-          },
-          {
-            m_childtitle: "Form validation",
-            m_childlink: "form-validation",
-          },
-          {
-            m_childtitle: "Wizard",
-            m_childlink: "form-wizard",
-          },
-          {
-            m_childtitle: "Input mask",
-            m_childlink: "input-mask",
-          },
-          {
-            m_childtitle: "File input",
-            m_childlink: "file-input",
-          },
-          {
-            m_childtitle: "Form repeater",
-            m_childlink: "form-repeater",
-          },
-          {
-            m_childtitle: "Textarea",
-            m_childlink: "textarea",
-          },
-          {
-            m_childtitle: "Checkbox",
-            m_childlink: "checkbox",
-          },
-          {
-            m_childtitle: "Radio button",
-            m_childlink: "radio-button",
-          },
-          {
-            m_childtitle: "Switch",
-            m_childlink: "switch",
-          },
-        ],
-      },
-      {
-        megamenutitle: "Utility",
-        megamenuicon: "heroicons-outline:user",
-        singleMegamenu: [
-          {
-            m_childtitle: "Invoice",
-            m_childlink: "invoice",
-          },
-          {
-            m_childtitle: "Pricing",
-            m_childlink: "pricing",
-          },
-          {
-            m_childtitle: "Testimonial",
-            m_childlink: "testimonial",
-          },
-          {
-            m_childtitle: "FAQ",
-            m_childlink: "faq",
-          },
-          {
-            m_childtitle: "Blog",
-            m_childlink: "blog",
-          },
-          {
-            m_childtitle: "404 page",
-            m_childlink: "/404",
-          },
-          {
-            m_childtitle: "Coming Soon",
-            m_childlink: "/coming-soon",
-          },
-          {
-            m_childtitle: "Under Maintanance page",
-            m_childlink: "/under-construction",
-          },
-        ],
-      },
-    ],
-  },
-
-  {
-    isHeadr: true,
-    title: "PAGES",
-  },
-  {
-    title: "Widgets",
-    icon: "heroicons-outline:view-grid-add",
-    link: "form-elements",
-    child: [
-      {
-        childtitle: "Basic",
-        childlink: "basic",
-        childicon: "heroicons-outline:document-text",
-      },
-      {
-        childtitle: "Statistic",
-        childlink: "statistic",
-        childicon: "heroicons-outline:document-text",
-      },
-    ],
-  },
-
-  {
-    title: "Extra",
-    icon: "heroicons-outline:template",
-
-    child: [
-      {
-        childtitle: "Basic Table",
-        childlink: "table-basic",
-        childicon: "heroicons-outline:table",
-      },
-      {
-        childtitle: "Advanced table",
-        childlink: "table-advanced",
-        childicon: "heroicons-outline:table",
-      },
-      {
-        childtitle: "Apex chart",
-        childlink: "appex-chart",
-        childicon: "heroicons-outline:chart-bar",
-      },
-      {
-        childtitle: "Chart js",
-        childlink: "chartjs",
-        childicon: "heroicons-outline:chart-bar",
-      },
-      {
-        childtitle: "Map",
-        childlink: "map",
-        childicon: "heroicons-outline:map",
-      },
-    ],
-  },
 ];
+
+// export const topMenu = [
+//   {
+//     isHeadr: true,
+//     title: "DASHBOARD",
+//   },
+//   {
+//     title: "Dashboard",
+//     icon: "heroicons-outline:home",
+//     link: "/app/home",
+//     child: [
+//       {
+//         childtitle: "Analytics Dashboard",
+//         childlink: "home",
+//         childicon: "heroicons:presentation-chart-line",
+//       },
+//       {
+//         childtitle: "Ecommerce Dashboard",
+//         childlink: "ecommerce",
+//         childicon: "heroicons:shopping-cart",
+//       },
+//       {
+//         childtitle: "Project  Dashboard",
+//         childlink: "project",
+//         childicon: "heroicons:briefcase",
+//       },
+//       {
+//         childtitle: "CRM Dashboard",
+//         childlink: "crm",
+//         childicon: "ri:customer-service-2-fill",
+//       },
+//       {
+//         childtitle: "Banking Dashboard",
+//         childlink: "banking",
+//         childicon: "heroicons:wrench-screwdriver",
+//       },
+//     ],
+//   },
+//   {
+//     title: "App",
+//     icon: "heroicons-outline:chip",
+//     link: "/app/home",
+//     child: [
+//       {
+//         childtitle: "Calendar",
+//         childlink: "calender",
+//         childicon: "heroicons-outline:calendar",
+//       },
+//       {
+//         childtitle: "Kanban",
+//         childlink: "kanban",
+//         childicon: "heroicons-outline:view-boards",
+//       },
+//       {
+//         childtitle: "Todo",
+//         childlink: "todo",
+//         childicon: "heroicons-outline:clipboard-check",
+//       },
+//       {
+//         childtitle: "Projects",
+//         childlink: "projects",
+//         childicon: "heroicons-outline:document",
+//       },
+//     ],
+//   },
+//   {
+//     title: "Pages",
+//     icon: "heroicons-outline:view-boards",
+//     link: "/app/home",
+//     megamenu: [
+//       {
+//         megamenutitle: "Authentication",
+//         megamenuicon: "heroicons-outline:user",
+//         singleMegamenu: [
+//           {
+//             m_childtitle: "Signin One",
+//             m_childlink: "/",
+//           },
+//           {
+//             m_childtitle: "Signin Two",
+//             m_childlink: "/login2",
+//           },
+//           {
+//             m_childtitle: "Signin Three",
+//             m_childlink: "/login3",
+//           },
+//           {
+//             m_childtitle: "Signup One",
+//             m_childlink: "/register",
+//           },
+//           {
+//             m_childtitle: "Signup Two",
+//             m_childlink: "/register/register2",
+//           },
+//           {
+//             m_childtitle: "Signup Three",
+//             m_childlink: "/register/register3",
+//           },
+//           {
+//             m_childtitle: "Forget Password One",
+//             m_childlink: "/forgot-password",
+//           },
+//           {
+//             m_childtitle: "Forget Password Two",
+//             m_childlink: "/forgot-password2",
+//           },
+//           {
+//             m_childtitle: "Forget Password Three",
+//             m_childlink: "/forgot-password3",
+//           },
+//           {
+//             m_childtitle: "Lock Screen One",
+//             m_childlink: "/lock-screen",
+//           },
+//           {
+//             m_childtitle: "Lock Screen Two",
+//             m_childlink: "/lock-screen2",
+//           },
+//           {
+//             m_childtitle: "Lock Screen Three",
+//             m_childlink: "/lock-screen3",
+//           },
+//         ],
+//       },
+
+//       {
+//         megamenutitle: "Components",
+//         megamenuicon: "heroicons-outline:user",
+//         singleMegamenu: [
+//           {
+//             m_childtitle: "typography",
+//             m_childlink: "typography",
+//           },
+//           {
+//             m_childtitle: "colors",
+//             m_childlink: "colors",
+//           },
+//           {
+//             m_childtitle: "alert",
+//             m_childlink: "alert",
+//           },
+//           {
+//             m_childtitle: "button",
+//             m_childlink: "button",
+//           },
+//           {
+//             m_childtitle: "card",
+//             m_childlink: "card",
+//           },
+//           {
+//             m_childtitle: "carousel",
+//             m_childlink: "carousel",
+//           },
+//           {
+//             m_childtitle: "dropdown",
+//             m_childlink: "dropdown",
+//           },
+//           {
+//             m_childtitle: "image",
+//             m_childlink: "image",
+//           },
+//           {
+//             m_childtitle: "modal",
+//             m_childlink: "modal",
+//           },
+//           {
+//             m_childtitle: "Progress bar",
+//             m_childlink: "progress-bar",
+//           },
+//           {
+//             m_childtitle: "Placeholder",
+//             m_childlink: "placeholder",
+//           },
+
+//           {
+//             m_childtitle: "Tab & Accordion",
+//             m_childlink: "tab-accordion",
+//           },
+//         ],
+//       },
+//       {
+//         megamenutitle: "Forms",
+//         megamenuicon: "heroicons-outline:user",
+//         singleMegamenu: [
+//           {
+//             m_childtitle: "Input",
+//             m_childlink: "input",
+//           },
+//           {
+//             m_childtitle: "Input group",
+//             m_childlink: "input-group",
+//           },
+//           {
+//             m_childtitle: "Input layout",
+//             m_childlink: "input-layout",
+//           },
+//           {
+//             m_childtitle: "Form validation",
+//             m_childlink: "form-validation",
+//           },
+//           {
+//             m_childtitle: "Wizard",
+//             m_childlink: "form-wizard",
+//           },
+//           {
+//             m_childtitle: "Input mask",
+//             m_childlink: "input-mask",
+//           },
+//           {
+//             m_childtitle: "File input",
+//             m_childlink: "file-input",
+//           },
+//           {
+//             m_childtitle: "Form repeater",
+//             m_childlink: "form-repeater",
+//           },
+//           {
+//             m_childtitle: "Textarea",
+//             m_childlink: "textarea",
+//           },
+//           {
+//             m_childtitle: "Checkbox",
+//             m_childlink: "checkbox",
+//           },
+//           {
+//             m_childtitle: "Radio button",
+//             m_childlink: "radio-button",
+//           },
+//           {
+//             m_childtitle: "Switch",
+//             m_childlink: "switch",
+//           },
+//         ],
+//       },
+//       {
+//         megamenutitle: "Utility",
+//         megamenuicon: "heroicons-outline:user",
+//         singleMegamenu: [
+//           {
+//             m_childtitle: "Invoice",
+//             m_childlink: "invoice",
+//           },
+//           {
+//             m_childtitle: "Pricing",
+//             m_childlink: "pricing",
+//           },
+//           {
+//             m_childtitle: "Testimonial",
+//             m_childlink: "testimonial",
+//           },
+//           {
+//             m_childtitle: "FAQ",
+//             m_childlink: "faq",
+//           },
+//           {
+//             m_childtitle: "Blog",
+//             m_childlink: "blog",
+//           },
+//           {
+//             m_childtitle: "404 page",
+//             m_childlink: "/404",
+//           },
+//           {
+//             m_childtitle: "Coming Soon",
+//             m_childlink: "/coming-soon",
+//           },
+//           {
+//             m_childtitle: "Under Maintanance page",
+//             m_childlink: "/under-construction",
+//           },
+//         ],
+//       },
+//     ],
+//   },
+
+//   {
+//     isHeadr: true,
+//     title: "PAGES",
+//   },
+//   {
+//     title: "Widgets",
+//     icon: "heroicons-outline:view-grid-add",
+//     link: "form-elements",
+//     child: [
+//       {
+//         childtitle: "Basic",
+//         childlink: "basic",
+//         childicon: "heroicons-outline:document-text",
+//       },
+//       {
+//         childtitle: "Statistic",
+//         childlink: "statistic",
+//         childicon: "heroicons-outline:document-text",
+//       },
+//     ],
+//   },
+
+//   {
+//     title: "Extra",
+//     icon: "heroicons-outline:template",
+
+//     child: [
+//       {
+//         childtitle: "Basic Table",
+//         childlink: "table-basic",
+//         childicon: "heroicons-outline:table",
+//       },
+//       {
+//         childtitle: "Advanced table",
+//         childlink: "table-advanced",
+//         childicon: "heroicons-outline:table",
+//       },
+//       {
+//         childtitle: "Apex chart",
+//         childlink: "appex-chart",
+//         childicon: "heroicons-outline:chart-bar",
+//       },
+//       {
+//         childtitle: "Chart js",
+//         childlink: "chartjs",
+//         childicon: "heroicons-outline:chart-bar",
+//       },
+//       {
+//         childtitle: "Map",
+//         childlink: "map",
+//         childicon: "heroicons-outline:map",
+//       },
+//     ],
+//   },
+// ];
 
 export const alertType = [
   {
