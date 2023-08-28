143c4d33aefcef08d05e322ded21394dc9b0a663
MahardiikaKD <dikamahar884@gmail.com>
2023-08-28 16:59:00 +0700

    initial template component

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,67 +1,11 @@
 <template>
 	<Card title="Tabs">
-		<TabGroup>
-			<TabList
-				class="lg:space-x-8 md:space-x-4 space-x-0 rtl:space-x-reverse">
-				<Tab
-					v-slot="{ selected }"
-					as="template"
-					v-for="(item, i) in buttons"
-					:key="i">
-					<button
-						:class="[
-							selected
-								? 'text-primary-500 before:w-full'
-								: 'text-slate-500 before:w-0 dark:text-slate-300',
-						]"
-						class="inline-flex items-start text-sm font-medium mb-7 capitalize bg-white dark:bg-slate-800 ring-0 foucs:ring-0 focus:outline-none px-2 transition duration-150 before:transition-all before:duration-150 relative before:absolute before:left-1/2 before:bottom-[-6px] before:h-[1.5px] before:bg-primary-500 before:-translate-x-1/2">
-						<span
-							class="text-base relative top-[1px] ltr:mr-1 rtl:ml-1"
-							><Icon :icon="item.icon" /></span
-						>{{ item.title }}
-					</button>
-				</Tab>
-			</TabList>
-			<TabPanels>
-				<TabPanel>
-					<div
-						class="text-slate-600 dark:text-slate-400 text-sm font-normal">
-						Ini Home
-					</div>
-				</TabPanel>
-				<TabPanel>
-					<div
-						class="text-slate-600 dark:text-slate-400 text-sm font-normal">
-						Aliqua id fugiat nostrud irure ex duis ea quis id quis
-						ad et. Sunt qui esse pariatur duis deserunt mollit
-						dolore cillum minim tempor enim.
-					</div>
-				</TabPanel>
-				<TabPanel
-					><div
-						class="text-slate-600 dark:text-slate-400 text-sm font-normal">
-						Aliqua id fugiat nostrud irure ex duis ea quis id quis
-						ad et. Sunt qui
-					</div></TabPanel
-				>
-				<TabPanel
-					><div
-						class="text-slate-600 dark:text-slate-400 text-sm font-normal">
-						Aliqua id fugiat nostrud irure ex duis ea quis id quis
-						ad et. Sunt qui esse pariatur duis deserunt mollit
-						dolore cillum minim tempor enim. Elit aute irure tempor
-						cupidatat incididunt sint deserunt ut voluptate aute id
-						deserunt nisi.
-					</div></TabPanel
-				>
-			</TabPanels>
-		</TabGroup>
+		<span>Views</span>
 	</Card>
 </template>
 
 <script setup>
 import Card from '@/components/Card';
-import { TabGroup, TabList, Tab, TabPanels, TabPanel } from '@headlessui/vue';
 import Icon from '@/components/Icon';
 const buttons = [
 	{
